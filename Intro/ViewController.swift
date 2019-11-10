e78865ce93fec6e6bf2ee7dd58816ae54e87ab1e
rustemsupayev <rustik7252@gmail.com>
2019-11-10 13:30:18 +0600

    Added a container view and additional string

--- Intro/ViewController.swift
+++ Intro/ViewController.swift
@@ -15,41 +15,67 @@ class ViewController: UIViewController {
     let TinderImageView: UIImageView = {
           let imageView = UIImageView(image: #imageLiteral(resourceName: "tinder1"))
           imageView.translatesAutoresizingMaskIntoConstraints = false
+          imageView.contentMode = .scaleAspectFit
           return imageView
     }()
 
     let descriptionTextView: UITextView = {
-       
+
         let textView = UITextView()
-        textView.text = "whatever for now"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        let attributedText = NSMutableAttributedString(string: "Whatever for now", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSAttributedString(string: "\n\nWhatever for now and later", attributes: [NSAttributedString.Key.font : UIFont.italicSystemFont(ofSize: 15),NSAttributedString.Key.foregroundColor : UIColor.red]))
+        
+        textView.attributedText = attributedText
+        
+//        textView.text = "whatever for now"
+//        textView.font = UIFont.boldSystemFont(ofSize: 18)
+//
         textView.textAlignment = .center
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.isEditable = false
         textView.isScrollEnabled = false
         return textView
     }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(TinderImageView)
+//        view.addSubview(TinderImageView)
         view.addSubview(descriptionTextView)
         setupLayout()
         
     }
     
     private func setupLayout() {
+        
+        
+             let topImageContainerView = UIView()
+              view.addSubview(topImageContainerView)
+            // enable auto layout
+             topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+             topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+             topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+             topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+              topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+             topImageContainerView.addSubview(TinderImageView)
+        
+        TinderImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        TinderImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        TinderImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
     
-             TinderImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-             TinderImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-             TinderImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
-             TinderImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
-    
-             descriptionTextView.topAnchor.constraint(equalTo: TinderImageView.bottomAnchor, constant: 100).isActive = true
-             descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-             descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+             descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+             descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+             descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
              descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+
+        
+        
 
     }
 }
